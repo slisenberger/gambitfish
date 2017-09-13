35241ec1db08a7458a9999bdb5876d7180cbad8c
stefan <slisenberger@gmail.com>
2017-09-12 23:53:58 -0700

    remove obv bad search code..

--- Gambitfish/engine/search/search.go
+++ Gambitfish/engine/search/search.go
@@ -7,25 +7,22 @@ import "../evaluate"
 
 // An Alpha Beta Negamax implementation. Function stolen from here:
 // https://en.wikipedia.org/wiki/Negamax#Negamax_with_alpha_beta_pruning
-func AlphaBetaSearch(b *game.Board, e evaluate.Evaluator, depth int, alpha, beta float64, initial bool) (float64, *game.Move) {
+func AlphaBetaSearch(b *game.Board, e evaluate.Evaluator, depth int, alpha, beta float64) (float64, *game.Move) {
 	if b.Finished() || depth == 0 {
-		b.SwitchActivePlayer()
 		return e.Evaluate(b), nil
 	}
 	_ = alpha
 	_ = beta
 	var best game.Move
 	var eval float64
-	// On recursive calls, we want to make sure we change who the player is for
-	// move generation and score evaluation.
 	moves := b.AllLegalMoves()
 	bestVal := math.Inf(-1)
 	for _, move := range moves {
-		newBoard := b.Copy()
+		newBoard := game.CopyBoard(b)
 		newBoard.ApplyMove(move)
 		newBoard.SwitchActivePlayer()
-		eval, _ = AlphaBetaSearch(newBoard, e, depth-1, -beta, -alpha, false)
-		eval = -1.0 * eval
+		eval, _ = AlphaBetaSearch(newBoard, e, depth-1, -beta, -alpha)
+		eval = -eval
 		if eval > bestVal {
 			fmt.Println(fmt.Sprintf("new best eval at depth %v!: %v, %v", depth, eval, move))
 			fmt.Println(fmt.Sprintf("above value is negative of value for %v", newBoard.Active))
--- Gambitfish/engine/search/search_test.go
+++ Gambitfish/engine/search/search_test.go
@@ -22,13 +22,13 @@ func TestSacrificeQueen(t *testing.T) {
 	b := initGreedyQueenBoard()
 	e := evaluate.MaterialEvaluator{}
 	// With one ply, the greedy option is take the pawn!
-	_, move := AlphaBetaSearch(b, e, 1, math.Inf(-1), math.Inf(1), true)
+	_, move := AlphaBetaSearch(b, e, 1, math.Inf(-1), math.Inf(1))
 	if move.String() != "Qb1 to b7" {
 		t.Errorf("queen did not take greedy option qb7, but instead %v", move)
 	}
 	// With two plies, it should be clear that taking the pawn is a bad
 	// sacrifice!
-	_, move = AlphaBetaSearch(b, e, 2, math.Inf(-1), math.Inf(1), true)
+	_, move = AlphaBetaSearch(b, e, 2, math.Inf(-1), math.Inf(1))
 	if move.String() == "Qb1 to b7" {
 		t.Error("Queen sacrificed on position with depth=2, which is losing")
 	}
